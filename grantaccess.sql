82c89ef1aa9684772f6c6be72ffc383ee725ed10
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2021-01-28 10:28:07 +0800

    initial commit

---
+++ grantaccess.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS dvd_rental;
+CREATE DATABASE dvd_rental;
+
+CREATE USER shihara WITH PASSWORD 'classified23';
+GRANT ALL PRIVILEGES ON DATABASE "dvd_rental" to shihara;
+
+
+
+
+
+
