37e8a8aabb8a0d4984393b4c3883af1e6addc559
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2021-01-30 04:25:58 +0800

    view implemented

---
+++ Views/views.sql
@@ -0,0 +1,11 @@
+create or replace view myview as 
+	SELECT film_id, title
+	FROM film f
+	WHERE film_id in(
+		SELECT film_id
+		FROM film_category fc 
+		HAVING count(film_id) > 1
+	)
+	INNER JOIN LANGUAGE l 
+	ON f.language_id = l.language_id;
+
